99c0b583e258d526543cf7b8ce65f66f14be1a92
jerick2209 <146998003+jerick2209@users.noreply.github.com>
2023-12-04 23:05:56 -0500

    Add files via upload

---
+++ src/util/getUserId.js
@@ -0,0 +1,12 @@
+import { jwtDecode } from "jwt-decode";
+
+
+export const getUserId = (token) => {
+    const decoded = jwtDecode(token)
+    return decoded._id
+}
+
+
+export const getUserIdFromLocalStorage = () => {
+    return localStorage.getItem("userId")
+}
